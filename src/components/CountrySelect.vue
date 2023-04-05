a965da585f68f1f17850104aabdb2c1cc4e08077
David Thumbi <davidthumbi78@gmail.com>
2023-04-05 10:19:20 +0300

    added project

---
+++ src/components/CountrySelect.vue
@@ -0,0 +1,27 @@
+<template>
+    <select @change="handleChange()" v-model="selected" class="form-select mt-10 block w-full border p-3 rounded mb-10">
+        <option value="0">Select Country</option>
+        <option v-for="country in countries" :value="country.ID">
+            {{ country.Country }}
+        </option>
+    </select>
+</template>
+
+<script>
+    export default {
+        name: 'CountrySelect',
+        props: ['countries'],
+        data() {
+            return {
+                selected: 0
+            }
+        },
+        methods: {
+            handleChange() {
+                const country = this.countries.find(country => country.ID === this.selected)
+
+                this.$emit('get-country', country)
+            }
+        }
+    }
+</script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="bg-gray-900 text-white text-center p-10 bottom-0">Covid 19 tracker</div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
